34ac5f1798523d9e0d0761a2ee660df20cdc45ed
ReedJordan <89485455+ReedJordan@users.noreply.github.com>
2024-10-15 13:11:16 -0700

    Update WordGamesTester.java
    syncing

--- src/word_games/WordGamesTester.java
+++ src/word_games/WordGamesTester.java
@@ -16,20 +16,34 @@ public class WordGamesTester {
         System.out.println(firstMix);
         // Ask for an index
         // Ask for random word
-        /*System.out.println("Enter an integer: ");
+        System.out.println("Enter an integer: ");
         int idx = input.nextInt();
         input.nextLine();  // skip over the newline
         System.out.println("Enter another word: ");
-        String word = input.nextLine();
-        */
+        String addWord = input.nextLine();
+        wordplay.bananaSplit(idx, addWord);
+        String secondMix = wordplay.bananaSplit(idx, addWord);
+        System.out.println(secondMix);
         // Add random word at index
         // Print out the word
-
+        
 
         // Ask for a character (store as a String)
         // Ask for random word
         // Add random word at character
         // Print out the word
-
+        System.out.println("Enter a character that is part of your word: ");
+        String pickChar = input.nextLine();
+        System.out.println("Enter another word: ");
+        String bonusWord = input.nextLine();
+        wordplay.bananaSplit(pickChar,bonusWord);
+        String thirdMix = wordplay.bananaSplit(pickChar,bonusWord);
+        System.out.println(thirdMix);
+        
+        
+        
+        System.out.println("This has been fun wordplay with [" text "]");
+        
+        
     }
 }
